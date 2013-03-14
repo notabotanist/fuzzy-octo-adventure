ad188e5bfd9c9a7e70cab4e3ed8f8c8e4746314d
Matt MacEwan(Desk) <mrm4677@rit.edu>
2013-03-13 23:48:58 -0400

    Added intersection tests for Ray-Triangle
    Changed result type of intersection to include normal.

--- Scene.hs
+++ Scene.hs
@@ -1,25 +1,30 @@
 module Scene where
 
---import Data.Vect () -- operators
+import Data.Vect ((&.),(&^),(&-),(&+)) -- operators (dot, cross)
 import qualified Data.Vect as Vect
 import qualified Geom
 import Control.Monad (guard, mplus)
+import Control.Applicative ((<$>))
+
+-- |Represents the intersection of a ray with an object by the
+-- omega distance to the intersection point and a normal at that point
+type Intersection = (Float, Vect.Normal3)
 
 -- |Class of objects which can be intersected by rays
 class Object a where
   -- |Intersects an Object with a Ray, and Maybe returns the parametric
   -- distance to the point of intersection
-  intersect :: a -> Geom.Ray -> Maybe Float
+  intersect :: a -> Geom.Ray -> Maybe Intersection
 
 -- |Sphere data structure, with center point and radius
 data Sphere = Sphere { center :: Geom.Point, radius :: Float }
 
 instance Object Sphere where
 --intersect :: Sphere -> Geom.Ray -> Maybe t
-  intersect (Sphere (Vect.Vec3 xc yc zc) r) (Geom.Ray origin norm)
+  intersect (Sphere c@(Vect.Vec3 xc yc zc) r) (Geom.Ray origin norm)
     | discrim < 0 = Nothing
-    | discrim == 0 = Just $ (-bB + discrim') / 2
-    | otherwise = ming0 ((-bB + discrim') / 2) ((-bB - discrim') / 2)
+--  | discrim == 0 = Just $ (-bB + discrim') / 2
+    | otherwise = pair <$> ming0 ((-bB + discrim') / 2) ((-bB - discrim') / 2)
     where
       (Vect.Vec3 x0 y0 z0) = origin
       (Vect.Vec3 dx dy dz) = Vect.fromNormal norm
@@ -29,3 +34,26 @@ instance Object Sphere where
       discrim' = sqrt discrim
       ming0 a b = (guard (min a b > 0) >> return (min a b)) `mplus`
                   (guard (max a b > 0) >> return (max a b))
+      pair t = (t, Vect.mkNormal((isect t) &- c))
+      isect t = (t `Vect.scalarMul` (Vect.fromNormal norm)) &+ origin
+
+-- |Triangle data structure, with 3 points
+data Triangle = Triangle Geom.Point Geom.Point Geom.Point
+
+instance Object Triangle where
+  intersect (Triangle p0 p1 p2) (Geom.Ray origin norm)
+    | divisor == 0 = Nothing
+    | t < 0 = Nothing
+    | u < 0 || v < 0 || u+v > 1 = Nothing
+    | otherwise = Just (t, norm')
+    where
+      e1 = p1 &- p0
+      e2 = p2 &- p0
+      vT = origin &- p0
+      vP = (Vect.fromNormal norm) &^ e2
+      vQ = vT &^ e1
+      divisor = vP &. e1
+      t = (vQ &. e2) / divisor
+      u = (vP &. vT) / divisor
+      v = (vQ &. (Vect.fromNormal norm)) / divisor
+      norm' = Vect.mkNormal(e1 &^ e2)
