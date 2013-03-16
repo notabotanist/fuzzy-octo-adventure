2a02d9a61573f5ea02ec9739b1af225eff4229a2
Matt MacEwan(Desk) <mrm4677@rit.edu>
2013-03-15 22:25:08 -0400

    Added assignment-specific camera

--- NabRay.hs
+++ NabRay.hs
@@ -10,6 +10,7 @@ import qualified Camera
 bgColor :: Scene.ColorF
 bgColor = (0, 0, 1)
 
+-- |Creates two Triangles representing a quad
 makeFloorQuad :: Geom.Point -- ^Center point
               -> Float      -- ^Half-width and half-height
               -> [Scene.Object]  -- ^Two triangles representing the quad
@@ -31,3 +32,9 @@ myScene = buildScene (Scene.ListScene bgColor []) where
   addFloor s = foldl Scene.addObject s (makeFloorQuad (Vect.Vec3 7 0 (-7)) 13)
   addMetal = addObject' (Scene.Sphere (Vect.Vec3 4 3 (-4)) 3)
   addClear = addObject' (Scene.Sphere (Vect.Vec3 0 5 0) 3)
+
+-- |Creates the camera transformation according to previously found values
+myCamera :: Camera.Camera
+myCamera = Camera.mkCamera (Vect.Vec3 0 5 9.4)
+            (Vect.mkNormal (Vect.Vec3 0 (-0.166769) (-0.98599604)))
+            (Vect.mkNormal (Vect.Vec3 0 1 0))
