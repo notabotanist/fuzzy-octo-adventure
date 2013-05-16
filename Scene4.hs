570dcaac46c979842b5a73456fca3a757eabde5a
Matt MacEwan(Desk) <mrm4677@rit.edu>
2013-05-16 14:40:21 -0400

    Added normal to result of intersect calculation

--- Scene4.hs
+++ Scene4.hs
@@ -72,14 +72,14 @@ toObjectCoords :: Object4 -> Vect.Vec4 -> Vect.Vec4
 toObjectCoords (Embed hp _ _) = Vect.extendZero.project hp
 toObjectCoords (HyperSphere c r) = Vect.scalarMul (1/r).(&- c)
 
-type Intersection = (Object4, Float)
+type Intersection = (Object4, Float, Vect.Normal4)
 
 intersect :: Geom4.Ray -> Object4 -> Maybe Intersection
 -- Embedded 3d object case
 intersect (Geom4.Ray p0 v) o@(Embed hp isContained _)
   | div == 0       = Nothing
   | t < 0          = Nothing
-  | isContained p' = Just (o, t)
+  | isContained p' = Just (o, t, vN)
   | otherwise      = Nothing
   where
   (HyperPlane r i j k vN) = hp
@@ -92,9 +92,9 @@ intersect (Geom4.Ray p0 v) o@(Embed hp isContained _)
   py = preIsect &. Vect.fromNormal j
   pz = preIsect &. Vect.fromNormal k
 -- Hypersphere case
-intersect (Geom4.Ray p0 v) o@(HyperSphere c r)
+intersect tr@(Geom4.Ray p0 v) o@(HyperSphere c r)
   | discrim < 0 = Nothing
-  | otherwise = (,) <$> pure o <*> nearT
+  | otherwise = (,,) <$> pure o <*> nearT <*> (iNorm <$> nearT)
   where
   p0' = p0 &- c
   sB = 2 * (p0' &. (Vect.fromNormal v))
@@ -104,6 +104,7 @@ intersect (Geom4.Ray p0 v) o@(HyperSphere c r)
   nearT = foldl findMin Nothing .filter (>0).map calcT $ [(+),(-)]
   findMin Nothing t = Just t
   findMin (Just a) b = Just (min a b)
+  iNorm t = Vect.mkNormal ((Geom4.eval tr t) &- c)
 
 transform :: Geom4.Transform -> Object4 -> Object4
 transform (Geom4.Transform rot trans) (Embed hp f g) = Embed hp' f g where
@@ -123,7 +124,7 @@ data Scene = Scene ColorF [Object4]
 trace :: Scene -> Geom4.Ray -> ColorF
 trace (Scene bg os) ray = case intersections of
   [] -> bg
-  ns -> let (o, t) = head ns in objColor o (Geom4.eval ray t)
+  ns -> let (o, t, _) = head ns in objColor o (Geom4.eval ray t)
   where
   intersections = mapMaybe (intersect ray) os
 
